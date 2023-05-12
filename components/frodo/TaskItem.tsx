ed1f6654736bed5c8bb7660b5d9874a4573641c5
Timmy <timmyjunchen@gmail.com>
2023-05-12 16:42:40 -0400

    Changed Promise for String

--- components/frodo/TaskItem.tsx
+++ components/frodo/TaskItem.tsx
@@ -1,17 +1,20 @@
 import { DeleteIcon } from "@chakra-ui/icons"
-import { Checkbox, HStack, IconButton, Text } from "@chakra-ui/react"
+import { Checkbox, HStack, IconButton, Text, Image } from "@chakra-ui/react"
 import { collection, deleteDoc, doc, updateDoc } from "firebase/firestore"
+import { getStorage, ref, getDownloadURL } from "firebase/storage"
 import { TaskWithId } from "../../types"
-import { db } from "../../util/firebase"
+import { db, storage } from "../../util/firebase"
+import { useState, useEffect } from "react";
+import { useInstantTransition } from "framer-motion"
 
 type Props = {
   readonly task: TaskWithId
 }
 
-const TaskItem = ({ task: { id, text, checked } }: Props) => { //TODO: CHANGE TO POST
+const TaskItem = ({ task: { id, text, lost, image, checked } }: Props) => { //TODO: CHANGE TO POST
   const toggleTask = () => {
     const taskDoc = doc(collection(db, "tasks"), id)
-    updateDoc(taskDoc, { checked: !checked })//TODO: change to found: !found ?
+    updateDoc(taskDoc, { lost: !lost })//TODO: change to found: !found ?
   }
 
   const deleteTask = () => {
@@ -19,12 +22,24 @@ const TaskItem = ({ task: { id, text, checked } }: Props) => { //TODO: CHANGE TO
     deleteDoc(taskDoc)
   }
 
+  const [downloardUrl, setDownloadUrl] = useState("")
+
+  useEffect(
+    () => { getDownloadURL(ref(storage, image)).then((value: string) => { console.log(value); setDownloadUrl(value) }).catch((err) => console.log(err)) }, []
+  )
+
+  // const uploadImg = () => {
+  //   // const taskDoc = doc(collection(db, "tasks"), id)
+  //   return getDownloadURL(ref(storage, image))
+  // }
+
   return (
     <HStack w="100%">
       <Checkbox isChecked={checked} onChange={toggleTask} />
       <Text textDecorationLine={checked ? "line-through" : "initial"}>
         {text}
       </Text>
+      <Image src={downloardUrl} />
       <IconButton
         aria-label="claim item"
         size="xs"
