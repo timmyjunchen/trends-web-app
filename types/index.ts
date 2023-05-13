ed1d64e676b2716a1e3e3336a09f50d6f3bbd3ae
Nidhi Mylavarapu <nm549@cornell.edu>
2023-05-12 22:39:25 -0400

    description

--- types/index.ts
+++ types/index.ts
@@ -3,7 +3,7 @@ import { StorageReference } from "firebase/storage"
 export type Task = {
   owner: string
   text: string
-  description: string
+  description: string //just added this
   lost: boolean
   image: string // <-- this needs to be a hash
   checked: boolean
